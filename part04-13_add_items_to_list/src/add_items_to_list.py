5df98e169782866f229e33633fcc408063a2a308
sernamalala <sm.sernamalala@gmail.com>
2025-02-21 23:43:10 +0200

    did a exercise

--- part04-13_add_items_to_list/src/add_items_to_list.py
+++ part04-13_add_items_to_list/src/add_items_to_list.py
@@ -1,1 +1,8 @@
 # Write your solution here
+
+items_no = int(input("How many items:"))
+my_list = []
+for i in range(items_no):
+    current_item = int(input(f"Item {i+1}:"))
+    my_list.append(current_item)
+print(my_list)
