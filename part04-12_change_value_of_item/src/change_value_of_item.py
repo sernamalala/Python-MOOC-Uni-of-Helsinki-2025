d252c741a46f13b1d91aa7834865b023fca15170
sernamalala <sm.sernamalala@gmail.com>
2025-02-21 23:12:55 +0200

    working with lists in python

--- part04-12_change_value_of_item/src/change_value_of_item.py
+++ part04-12_change_value_of_item/src/change_value_of_item.py
@@ -1,1 +1,18 @@
 # Write your solution here
+
+array = [1, 2, 3, 4, 5]
+
+while True:
+    index = int(input("Index:"))
+    
+    if index==-1:
+        break
+    
+    elif index>=0:
+        new_value = int(input("New value:"))
+        array[index] = new_value
+    elif index<0:
+        new_value = int(input("New value:"))
+        array.append(new_value)
+    
+    print(array)
