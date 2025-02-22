28f509e2fbe34f36a5b800822c1f5d026ecfed33
sernamalala <sm.sernamalala@gmail.com>
2025-02-22 11:10:32 +0200

    last exercises based on lists

--- part04-17_length_of_list/src/length_of_list.py
+++ part04-17_length_of_list/src/length_of_list.py
@@ -1,4 +1,6 @@
 # Write your solution here
+def length(list_input):
+    return len(list_input)
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
--- part04-18_mean/src/mean.py
+++ part04-18_mean/src/mean.py
@@ -1,4 +1,6 @@
 # Write your solution here
+def mean(input):
+    return sum(input)/len(input)
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
