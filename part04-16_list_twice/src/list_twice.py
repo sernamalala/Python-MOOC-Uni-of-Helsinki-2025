10ff779b2c1f5cf61134c13bffd184bbb78528ab
sernamalala <sm.sernamalala@gmail.com>
2025-02-22 11:01:06 +0200

    couple more exercises

--- part04-16_list_twice/src/list_twice.py
+++ part04-16_list_twice/src/list_twice.py
@@ -1,1 +1,13 @@
 # Write your solution here
+
+array = []
+while True:
+    new_item = int(input("New item:"))
+    if new_item!=0:
+        array.append(new_item)
+        print(f"The list now: {array}")
+        print(f"The list in order: {sorted(array)}")
+
+    elif new_item==0:
+        print("Bye!")
+        break
