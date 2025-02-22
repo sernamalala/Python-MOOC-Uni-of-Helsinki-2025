031071d7f674bd2fb44fb073d4b471c30125d87e
sernamalala <sm.sernamalala@gmail.com>
2025-02-22 10:56:11 +0200

    Did some exercises babased on lists

--- part04-14_addition_and_removal/src/addition_and_removal.py
+++ part04-14_addition_and_removal/src/addition_and_removal.py
@@ -1,1 +1,22 @@
 # Write your solution here
+array = []
+num = 1
+while True:
+    print(f"The list is now {array}")
+    input_command = input("a(d)d, (r)emove or e(x)it:")
+
+    if input_command == "d":
+        array.append(num)
+        num+=1
+    elif input_command == "r":
+        array.pop()
+        num-=1
+    if input_command == "x":
+        print("Bye!")
+        break
+        
+    
+
+
+    
+    
--- part04-15_same_word_twice/src/same_word_twice.py
+++ part04-15_same_word_twice/src/same_word_twice.py
@@ -1,1 +1,12 @@
 # Write your solution here
+
+array = []
+while True:
+    word = input("Word: ")
+    if word not in array:
+        array.append(word)
+    else:
+        print(f"You typed in {len(array)} different words")
+        break
+
+    
