b885b9532d87cc6c981e946bcee73fadab8c167b
sernamalala <sm.sernamalala@gmail.com>
2025-02-24 10:58:25 +0200

    reversed list exercise

--- part04-33_everything_reversed/src/everything_reversed.py
+++ part04-33_everything_reversed/src/everything_reversed.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def everything_reversed(array):
+    word = ""
+    new_array = []
+    for i in range(len(array)-1,-1,-1):
+        for j in range(len(array[i])-1,-1,-1):
+            print(array[i][j],end="")
+            word+=array[i][j]
+        print("")
+        new_array.append(word)
+        word = ""
+    return new_array
