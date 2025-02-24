fb84a4e6facda1ba569edb839394ef54c8eab842
sernamalala <sm.sernamalala@gmail.com>
2025-02-24 11:14:01 +0200

    most common character using loops

--- part04-34_most_common_character/src/most_common_character.py
+++ part04-34_most_common_character/src/most_common_character.py
@@ -1,1 +1,11 @@
 # Write your solution here
+
+def most_common_character(string):
+    highest = 0
+
+    for i in range(len(string)):
+        count = string.count(string[i])
+        if count>highest:
+            highest=count
+            char = string[i]
+    return  char       
