261f987786dafccd0136749b64937872a60252ca
sernamalala <sm.sernamalala@gmail.com>
2025-02-24 11:20:13 +0200

    vowels exercise

--- part04-35_no_vowels_allowed/src/no_vowels_allowed.py
+++ part04-35_no_vowels_allowed/src/no_vowels_allowed.py
@@ -1,1 +1,11 @@
 # Write your solution here
+
+
+
+def no_vowels(string):
+    vowels = ['a','e','i','o','u']
+    new_string = ""
+    for i in range(len(string)):
+        if(string[i] not in vowels):
+            new_string+=string[i]
+    return new_string
