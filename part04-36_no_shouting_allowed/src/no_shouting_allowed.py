68346dcd42729ceeb6f749868a9f2d3b915f822c
sernamalala <sm.sernamalala@gmail.com>
2025-02-25 08:51:36 +0200

    upper meathods in python

--- part04-36_no_shouting_allowed/src/no_shouting_allowed.py
+++ part04-36_no_shouting_allowed/src/no_shouting_allowed.py
@@ -1,1 +1,9 @@
 # Write your solution here
+
+def no_shouting(my_list):
+    new_list = []
+    for i in my_list:
+        if i.isupper() == False:
+            new_list.append(i)
+    return new_list
+
