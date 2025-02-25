cb687f15e200f6137096d07d5128813c48195af5
sernamalala <sm.sernamalala@gmail.com>
2025-02-25 09:21:45 +0200

    neigbours loops exercise

--- part04-37_neighbours_in_list/src/neighbours_in_list.py
+++ part04-37_neighbours_in_list/src/neighbours_in_list.py
@@ -1,1 +1,17 @@
 # Write your solution here
+
+def longest_series_of_neighbours(my_list):
+    if not my_list:
+        return 0
+    
+    count = 1
+    max_count = 1
+
+    for i in range(1,len(my_list)):
+        if abs(my_list[i] - my_list[i-1]) == 1:
+            count+=1
+        else:
+            max_count = max(max_count,count)
+            count = 1
+    max_count = max(max_count,count)
+    return max_count
