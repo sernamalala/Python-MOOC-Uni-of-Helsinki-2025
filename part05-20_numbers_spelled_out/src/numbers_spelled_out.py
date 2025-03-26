3a31e71c37136d73ea32c514c38c05a758c9d8f2
sernamalala <sm.sernamalala@gmail.com>
2025-03-26 19:10:37 +0200

    exercise done

--- part05-20_numbers_spelled_out/src/numbers_spelled_out.py
+++ part05-20_numbers_spelled_out/src/numbers_spelled_out.py
@@ -2,25 +2,23 @@
 
 def dict_of_numbers():
     current = ""
-    my_numbers = {0:"zero",1:"one",2:"two",3:"three",4:"four",5:"five",6:"six",7:"seven",8:"eight",9:"nine", 10:"ten", 20:"twenty",30:"thirty",40:"forty",50:"fifty",60:"sixty",70:"seventy",80:"eighty",90:"ninety"}
+    my_numbers = {
+        0: "zero", 1: "one", 2: "two", 3: "three", 4: "four",
+        5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine",
+        10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen",
+        15: "fifteen", 16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen",
+        20: "twenty", 30: "thirty", 40: "forty", 50: "fifty",
+        60: "sixty", 70: "seventy", 80: "eighty", 90: "ninety"
+    }
 
-    for input_val in range(11,100):
+    for input_val in range(21,100):
+        tens = (input_val//10)*10
+        ones = input_val %10
 
-        if input_val>10 and input_val<=99:
+        if ones == 0:
+            continue
 
-            new_i = int(str(input_val)[0]+"0")
-            current+=my_numbers[new_i]
-            current+="-"
-            val2 = int(str(input_val)[1])
-            current+=my_numbers[val2]
-            my_numbers[new_i] =current
+        my_numbers[input_val] = my_numbers[tens] + "-" + my_numbers[ones]
+
     return my_numbers
         
-        
-
-numbers = dict_of_numbers()
-print(numbers[2])
-print(numbers[11])
-print(numbers[45])
-print(numbers[99])
-print(numbers[0])
